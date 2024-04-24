1e8de0a488a393bdb60f29ae0db6c2a3a15dee11
Matthew Newville <newville@cars.uchicago.edu>
2024-04-24 16:40:52 -0500

    do not need desc in motor fields -- always added for any PV

--- epicsarchiver/util.py
+++ epicsarchiver/util.py
@@ -23,7 +23,7 @@ MAX_EPOCH = 2147483647.0   # =  2**31 - 1.0 (max unix timestamp)
 SEC_DAY   = 86400.0
 
 motor_fields = ('.VAL','.OFF','.FOFF','.SET','.HLS','.LLS',
-                '.DIR','_able.VAL','.SPMG','.DESC')
+                '.DIR','_able.VAL','.SPMG')
 
 valid_pvstr = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789:._-+:[]<>;{}'
 
